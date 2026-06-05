ba496093669be77616a448fa3e26413a95b29a50
Pancake <2514765066@qq.com>
2026-06-05 13:16:39 +0800

    [新增]：新增快捷键Ctrl+N新建工作空间

--- src/renderer/src/layout/side-bar/workspace/workspace-header.vue
+++ src/renderer/src/layout/side-bar/workspace/workspace-header.vue
@@ -2,22 +2,32 @@
   <section class="pl-3 pr-2 flex items-center">
     <span class="text-xs text-muted-foreground">工作空间</span>
 
-    <Button class="ml-auto" variant="outline" size="xs" @click="handleAdd">
-      <PlusIcon class="size-4" />
+    <Tooltip label="新建工作空间 Ctrl+N" side="bottom">
+      <Button class="ml-auto" variant="outline" size="xs" @click="handleAdd">
+        <PlusIcon class="size-4" />
 
-      新空间
-    </Button>
+        新空间
+      </Button>
+    </Tooltip>
   </section>
 </template>
 
 <script setup lang="ts">
+import Tooltip from "@/components/tooltip.vue";
 import { Button } from "@/components/ui/button";
 import eventEmitter from "@/hooks/eventEmitter";
+import { useEventListener } from "@vueuse/core";
 import { PlusIcon } from "lucide-vue-next";
 
 const handleAdd = () => {
   eventEmitter.emit("dialog-workspace-add:show");
 };
+
+useEventListener("keydown", e => {
+  if (e.ctrlKey && e.key == "n") {
+    handleAdd();
+  }
+});
 </script>
 
 <style scoped lang="scss"></style>
