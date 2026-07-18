192228a23f996c77f35f297bc50ae34f9ce27914
张铭洋 <2514765066@qq.com>
2026-07-18 18:50:01 +0800

    [优化]：提高解析大文档的速度

--- src/main/utils/md5.ts
+++ src/main/utils/md5.ts
@@ -1,13 +1,13 @@
-import { readFile } from 'fs/promises';
+import { createReadStream } from 'fs';
 import { createHash } from 'crypto';
 
 export const getMd5 = async (path: string) => {
   try {
-    const buffer = await readFile(path);
-
     const hash = createHash('md5');
 
-    hash.update(buffer);
+    for await (const chunk of createReadStream(path)) {
+      hash.update(chunk);
+    }
 
     return hash.digest('hex');
   } catch {
